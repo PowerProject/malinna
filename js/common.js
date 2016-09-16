5fbf037903b6d4420a9a4fd69da1087fd0b58573
kovalyova-alena <kovalyova_alena@inbox.ru>
2016-09-16 14:40:31 +0300

    create set timeout function. add animation to preloader

--- js/common.js
+++ js/common.js
@@ -1,14 +1,21 @@
-/**
- * Created by Kovalyova Alena on 9/15/2016.
- */
+"use strict";
 
 $(document).ready(function() {
+
+    function hinePreloader() {
+        $('.page-loader').css('display','none');
+    }
+
+
     var settings = function () {
         var wh=$(window).height();
         $('.page-loader').css({'height': wh + 'px'});
         return wh;
     };
+
+    setTimeout(hinePreloader, 3000);
     settings();
+
 
 
 
